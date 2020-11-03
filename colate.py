d9c1358cd020638c2c4fcfe807ab9f35f472a57a
kmwater <72469238+kmwater@users.noreply.github.com>
2020-11-03 10:25:05 -0500

    Add files via upload

--- colate.py
+++ colate.py
@@ -1,13 +1,17 @@
 import pandas as pd
+import numpy as np
 
 def put_together(city):
     weather = pd.read_csv('cleaned_data/'+city+'weather_uptd.csv',sep=",",usecols=('HourlyDewPointTemperature','HourlyDryBulbTemperature','HourlyPrecipitation','HourlyRelativeHumidity','HourlyVisibility','HourlyWetBulbTemperature','HourlyWindGustSpeed','HourlyWindSpeed'))
     dates = pd.read_csv('cleaned_data/'+city+'_rph.csv',sep = ',',usecols=('month','day','hour'))
+    census = pd.read_csv('cleaned_data/'+city+'_census.csv', sep = ',')
+
     rph = pd.read_csv('cleaned_data/'+city+'_rph.csv',sep=",", usecols=['ridesperhour'])
-    new = pd.concat((dates,weather,rph),axis=1)
+    new = pd.concat((dates,weather,census,rph),axis=1)
+
     pd.DataFrame.to_csv(new, city+"data.csv", sep = ",")
 
-    return()
+    return(new)
 
 put_together('Boston')
 put_together('DC')
@@ -15,3 +19,10 @@ put_together('Philadelphia')
 put_together('NYC')
 put_together('LA')
 put_together('Chicago')
+
+def total():
+    a = np.vstack((put_together('Boston'),put_together('DC'),put_together('Philadelphia'),put_together('LA'),put_together('NYC'), put_together('Chicago')))
+    np.savetxt('totaldata.csv',a, delimiter = ',')
+    return()
+
+total()
