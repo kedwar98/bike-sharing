5013fb4b63ebdfc899bdc1297d7db562cc56c91f
kmwater <72469238+kmwater@users.noreply.github.com>
2020-11-09 15:37:54 -0500

    Add files via upload

--- models/models_code_upd.py
+++ models/models_code_upd.py
@@ -9,7 +9,7 @@
 #Run this cell for import statements
 
 
-import pylab
+
 import calendar
 import numpy as np
 import pandas as pd
@@ -21,11 +21,11 @@ from datetime import datetime
 import matplotlib
 import matplotlib.pyplot as plt
 import warnings
-from itertools import groupby  
-from itertools import zip_longest 
+from itertools import groupby
+from itertools import zip_longest
 pd.options.mode.chained_assignment = None
 warnings.filterwarnings("ignore", category=DeprecationWarning)
-%matplotlib inline
+#matplotlib inline
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_validate
 from sklearn import metrics
@@ -41,21 +41,21 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 import eli5
 from eli5.sklearn import PermutationImportance
-
+import math
 
 
 
 #%% Mean squared error
 
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_log_error
 
 #mean_squared_error(y_true, y_pred, squared=False)
 #Set squared=False FOR RMSE
 
-
 #%%
 #COMBINED
-
+totaldata = pd.read_csv('WEATHER-CENSUS/totaldata.csv')
 dfb=totaldata.copy()
 
 #randomly shuffle data, as we are predicting rides per hour and not explicitly forecasting
@@ -67,30 +67,30 @@ dfbk=KFold(n_splits=5,shuffle=True, random_state=None)
 #make an 80:20 split out of 8760x6 rows
 datatrain=dfb.copy()
 testdata=dfb.copy()
-testdata=dfb[42000:-1]
-datatrain=dfb[0:42000]
-datatrainall=dfb[0:42000]
-testdataall=dfb[42000:-1]
+testdata=dfb[42048:-1]
+datatrain=dfb[0:42048]
+datatrainall=dfb[0:42048]
+testdataall=dfb[42048:-1]
 
 
-#%%
-#CITY
-
-dfb=Bostondata.copy()    #change city name here
-
-#randomly shuffle data, as we are predicting rides per hour and not explicitly forecasting
-dfb = dfb.sample(frac=1).reset_index(drop=True)
-#could also use sklearn.model_selection.KFold
-
-dfbk=KFold(n_splits=5,shuffle=True, random_state=None)
-
-#make an 80:20 split out of 8760 rows
-datatrain=dfb.copy()
-testdata=dfb.copy()
-testdata=dfb[7000:-1]
-datatrain=dfb[0:6999]
-datatrainall=dfb[0:6999]
-testdataall=dfb[7000:-1]
+# #%%
+# #CITY
+# DCdata = pd.read_csv("WEATHER/DC_norm.csv")
+# dfb=DCdata.copy()    #change city name here
+#
+# #randomly shuffle data, as we are predicting rides per hour and not explicitly forecasting
+# dfb = dfb.sample(frac=1).reset_index(drop=True)
+# #could also use sklearn.model_selection.KFold
+#
+# dfbk=KFold(n_splits=5,shuffle=True, random_state=None)
+#
+# #make an 80:20 split out of 8760 rows
+# datatrain=dfb.copy()
+# testdata=dfb.copy()
+# testdata=dfb[7008:-1]
+# datatrain=dfb[0:7008]
+# datatrainall=dfb[0:7008]
+# testdataall=dfb[7008:-1]
 
 
 #%%
@@ -110,6 +110,7 @@ for train_index, test_index in dfbk.split(dfb):
 
 #Unsplit (Keras will do validation splits)
 dataall=dfb.copy()
+rphall = dataall['ridesperhour']
 dataall.drop('ridesperhour',axis=1,inplace=True)
 dataall.drop(datatrain.columns[datatrain.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
 
@@ -141,10 +142,6 @@ testdata.drop('ridesperhour',axis=1,inplace=True)
 testdata.drop(testdata.columns[testdata.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
 
 
-
-
-
-
 #%%
 
 #Can use this for normalization / data transformation
@@ -162,10 +159,10 @@ datatesttransform=scaler.transform(testdata)
 
 #%%
 
-#Random Forest Model from sklearn
+# #Random Forest Model from sklearn
 
 #Here you can change number of trees, add other arguments, and change verbosity (2 tells you progress of tree building)
-rfModel = RandomForestRegressor(n_estimators=1000,verbose=2)
+rfModel = RandomForestRegressor(n_estimators=1000,verbose=1)
 
 
 
@@ -177,22 +174,28 @@ rfModel.fit(datatrainwo,yLabels)
 
 #training data model predictions using 80-20 split
 preds = rfModel.predict(X=datatrainwo)
+for i in range(len(preds)): #change
+     if preds[i] < 0:
+          preds[i] = 0
 #Change datatrainwo to datatrainwo to include census data
 
-print ("RF Training RMSE: ",mean_squared_error(yLabels, preds, squared=False)
-
-print ("RF Training Normalized RMSE: ",mean_squared_error(yLabels, preds, squared=False)/(max(yLabels)-min(yLabels)))
+print("RF Training RMSLE: ",math.sqrt(mean_squared_log_error(yLabels, preds)))
+print("RF Training RMSE:",mean_squared_error(yLabels,preds,squared=False))
+print("RF Training Normalized RMSE: ",mean_squared_error(yLabels, preds,squared = False)/(max(yLabels)-min(yLabels)))
 
 
 #test data model predictions using 80-20 split
 testp=rfModel.predict(X=testdatawo)
 #Change testdatawo to testdatawi to include census data
+for i in range(len(testp)):
+     if testp[i] < 0:
+          testp[i] = 0
 
 yLabelstest = testdataall['ridesperhour']
 
-print ("RF Testing RMSE: ",mean_squared_error(yLabelstest, testp, squared=False)
-
-print ("RF Testing Normalized RMSE: ",mean_squared_error(yLabelstest, testp, squared=False)/(max(yLabelstest)-min(yLabelstest)))
+print("RF Testing RMSLE: ",math.sqrt(mean_squared_log_error(yLabelstest, testp)))
+print("RF Testing RMSE:",mean_squared_error(yLabelstest, testp,squared=False))
+print("RF Testing Normalized RMSE: ",mean_squared_error(yLabelstest, testp, squared=False)/(max(yLabels)-min(yLabels)))
 
 
 #interesting parameter that lets you see relative contributions of features to predictions
@@ -214,25 +217,32 @@ plt.show()
 #Gradient Boosting Model from sklearn
 
 #Here you can change the number of estimators, the L1 regularization coefficient alpha, and other arguments, as well as verbosity
-gbModel = GradientBoostingRegressor(n_estimators=4000,alpha=0.01,verbose=2)
+gbModel = GradientBoostingRegressor(n_estimators=1000,alpha=0.01,verbose=0)
 yLabels = datatrainall['ridesperhour']
 
-#yLabelsLog = np.log1p(yLabels)
-gbModel.fit(datatrain,yLabelsLog)
-#Change datatrainwo to datatrainwo to include census data
+yLabelsLog = np.log1p(yLabels)
+gbModel.fit(datatrainwo,yLabels)
+#Change datatrainwo to datatrain to include census data
 
-preds = gbModel.predict(X= datatrain)
+preds = gbModel.predict(X= datatrainwo)
+for i in range(len(preds)):
+     if preds[i] < 0:
+          preds[i] = 0
 
-print ("GB Testing RMSE: ",mean_squared_error(yLabels, preds, squared=False)
 
-print ("GB Testing Normalized RMSE: ",mean_squared_error(yLabels, preds, squared=False)/(max(yLabels)-min(yLabels)))
+print ("GB Training RMSE: ",mean_squared_error(yLabels, preds, squared=False))
+print ("GB Training RMSLE: ",math.sqrt(mean_squared_log_error(yLabels, preds)))
+print("GB Training Normalized RMSE: ",mean_squared_error(yLabels, preds, squared=False)/(max(yLabels)-min(yLabels)))
 
-testp=gbModel.predict(X=testdata)
+testp=gbModel.predict(X=testdatawo)
 #Change testdatawo to testdatawi to include census data
+for i in range(len(testp)):
+     if testp[i] < 0:
+          testp[i] = 0
+yLabelstest = testdataall['ridesperhour']
 
-yLabelstest = np.log1p(testdataall['ridesperhour'])
-
-print ("GB Testing RMSE: ",mean_squared_error(yLabelstest, testp, squared=False)
+print ("GB Testing RMSE: ",mean_squared_error(yLabelstest, testp, squared=False))
+print("GB Testing RMSLE:", math.sqrt(mean_squared_log_error(yLabelstest, testp)))
 
 print ("GB Testing Normalized RMSE: ",mean_squared_error(yLabelstest, testp, squared=False)/(max(yLabelstest)-min(yLabelstest)))
 
@@ -256,7 +266,7 @@ plt.show()
 #%%
 
 #k-fold cross-validation using MSLE
-cval = cross_validate(rfModel, dataall, rphall['ridesperhour'], scoring=metrics.make_scorer(metrics.mean_squared_log_error), cv=10)
+cval = cross_validate(rfModel, dataall,rphall, scoring=metrics.make_scorer(metrics.mean_squared_log_error), cv=10)
 
 print(cval['test_score'])
 
@@ -278,11 +288,11 @@ model.add(k.layers.Dense(1))   #output predicted rides per hour
 
 #Adam optimizer is used for preliminary results. Other optimizers may be more stable/robust, perhaps less efficient
 #Use Keras MSLE metric
-model.compile(optimizer='adam',loss=tf.keras.losses.RootMeanSquaredError())
+model.compile(optimizer='adam',loss=tf.keras.losses.MeanSquaredError()) #I think this is MeanSquaredError function
 
 #Adjust number of epochs (iterations over set), validation split, other arguments, and verbosity
 #Change input as needed
-history = model.fit(dataallwo, yLabels, validation_split=0.2, epochs=200,verbose=2)   
+history = model.fit(dataallwo, yLabels, validation_split=0.2, epochs=200,verbose=0)
 #Change dataallwo to dataall to include census data
 
 
@@ -317,8 +327,5 @@ plt.show()
 
 permut = PermutationImportance(model, scoring="accuracy").fit(testdata,yLabels)
 eli5.show_weights(permut, feature_names = dataall.columns.tolist())
-
-
-
 
 
